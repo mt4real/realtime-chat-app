e55c220d1295557674297d27ac573c18731ee295
mt4real <54484589+mt4real@users.noreply.github.com>
2021-05-29 11:00:12 +0100

    Afolabi real time chat app

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express=require('express');
+const path=require('path');
+
+
+const app=express();
+const http=require('http').createServer(app);
+
+const PORT=process.env.PORT||8001;
+
+app.use(express.static(path.join(__dirname, 'public')))
+const io=require('socket.io')(http);
+io.on('connection', socket=>{
+
+    console.log('Connection ready')
+
+    socket.on('sendMessage', msg=>{
+        //console.log(msg)
+        socket.broadcast.emit('sendToAll',msg)
+    })
+})
+http.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
